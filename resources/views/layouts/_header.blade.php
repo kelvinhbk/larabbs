8a20d4c51302291a8dddbfeab8d2471027604b6c
vagrant <vagrant@homestead>
2019-03-09 10:49:10 +0000

    add 验证码

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -5,7 +5,7 @@
             LaraBBS
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
-        <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
@@ -14,8 +14,33 @@
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav navbar-right">
                 <!-- Authentication Links -->
-                <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
-            </ul> </div>
+                @guest
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+                @else
+                    <li class="nav-item dropdown">
+                        {{--<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">--}}
+                            {{--{{ Auth::user()->name }}--}}
+                        {{--</a>--}}
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
+                                <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
+                            </span>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown"><a class="dropdown-item" href="">个人中心</a>
+                            <a class="dropdown-item" href="">编辑资料</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" id="logout" href="#">
+                                <form action="{{ route('logout') }}" method="POST">
+                                    {{ csrf_field() }}
+                                    <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                                </form>
+                            </a>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
     </div>
 </nav>
